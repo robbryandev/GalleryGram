09484df693fc23a626109c6726537bb25a1a1a46
robbryandev <robbryandev@gmail.com>
2023-01-23 16:46:26 -0800

    WIP Add user seeding
    Co-Authored-By: David Gamble <daviddgamble@gmail.com>

--- GalleryGramApp/Migrations/20230124004202_AddSeeding.Designer.cs
+++ GalleryGramApp/Migrations/20230124004202_AddSeeding.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GalleryGram.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GalleryGram.Migrations
 {
     [DbContext(typeof(GalleryGramContext))]
-    partial class GalleryGramContextModelSnapshot : ModelSnapshot
+    [Migration("20230124004202_AddSeeding")]
+    partial class AddSeeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- GalleryGramApp/Models/GalleryGramContext.cs
+++ GalleryGramApp/Models/GalleryGramContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,5 +10,83 @@ namespace GalleryGram.Models
     public DbSet<Picture> Pictures { get; set; }
 
     public GalleryGramContext(DbContextOptions options) : base(options) { } 
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();  
+      
+      // Guid user1GID = Guid.NewGuid();
+      // string user1ID = user1GID.ToString();
+      ApplicationUser user1 = new ApplicationUser();
+        // user1.Id = user1ID;
+        user1.UserName = "david@gmail.com";
+        // user1.Email = "david@gmail.com";
+        user1.PasswordHash = passwordHasher.HashPassword(user1, "password"); 
+      // Guid user2GID = Guid.NewGuid();
+      // string user2ID = user1GID.ToString();
+      ApplicationUser user2 = new ApplicationUser();
+        // user2.Id = user2ID;
+        user2.UserName = "robert@gmail.com";
+        // user2.Email = "robert@gmail.com";
+        user2.PasswordHash = passwordHasher.HashPassword(user2, "password"); 
+      // Guid user3GID = Guid.NewGuid();
+      // string user3ID = user1GID.ToString();
+      ApplicationUser user3 = new ApplicationUser();
+        // user3.Id = user3ID;
+        user3.UserName = "lucas@gmail.com";
+        // user3.Email = "lucas@gmail.com";
+        user3.PasswordHash = passwordHasher.HashPassword(user3, "password"); 
+
+      List<ApplicationUser> users = new List<ApplicationUser>{ user1, user2, user3 };
+
+      builder.Entity<ApplicationUser>()
+        .HasData(users);
+
+      Picture pic1 = new Picture();
+        pic1.picture_id = 1;
+        pic1.user_id = user1.Id;
+        pic1.fileName = "https://via.placeholder.com/600/92c952";
+      Picture pic2 = new Picture();
+        pic2.picture_id = 2;
+        pic2.user_id = user1.Id;
+        pic2.fileName = "https://via.placeholder.com/600/771796";
+      Picture pic3 = new Picture();
+        pic3.picture_id = 3;
+        pic3.user_id = user1.Id;
+        pic3.fileName = "https://via.placeholder.com/600/24f355";
+
+      Picture pic4 = new Picture();
+        pic4.picture_id = 4;
+        pic4.user_id = user2.Id;
+        pic4.fileName = "https://via.placeholder.com/600/d32776";
+      Picture pic5 = new Picture();
+        pic5.picture_id = 5;
+        pic5.user_id = user2.Id;
+        pic5.fileName = "https://via.placeholder.com/600/f66b97";
+      Picture pic6 = new Picture();
+        pic6.picture_id = 6;
+        pic6.user_id = user2.Id;
+        pic6.fileName = "https://via.placeholder.com/600/56a8c2";
+      
+      Picture pic7 = new Picture();
+        pic7.picture_id = 7;
+        pic7.user_id = user2.Id;
+        pic7.fileName = "https://via.placeholder.com/600/b0f7cc";
+      Picture pic8 = new Picture();
+        pic8.picture_id = 8;
+        pic8.user_id = user2.Id;
+        pic8.fileName = "https://via.placeholder.com/600/54176f";
+      Picture pic9 = new Picture();
+        pic9.picture_id = 9;
+        pic9.user_id = user2.Id;
+        pic9.fileName = "https://via.placeholder.com/600/51aa97";
+
+      List<Picture> pictures = new List<Picture> { pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9 };
+
+      builder.Entity<Picture>()
+        .HasData(pictures);
+      
+      base.OnModelCreating(builder);
+    }
   }
 }
